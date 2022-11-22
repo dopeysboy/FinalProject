d9b504c1fabc2977942a956b90ef68baef8ffdbb
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-11-22 18:43:11 -0500

    create entity junit tests with em/emfs

---
+++ RotahuJPA/src/test/java/com/skilldistillery/rotahu/entities/ExpenseTests.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.rotahu.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ExpenseTests {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Expense expense;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("RotahuJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		expense = em.find(Expense.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		expense = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(expense);
+//		assertEquals("", expense.get);
+	}
+
+}
---
+++ RotahuJPA/src/test/java/com/skilldistillery/rotahu/entities/RatingTests.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.rotahu.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RatingTests {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Rating rating;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("RotahuJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		rating = em.find(Rating.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		rating = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(rating);
+//		assertEquals("", rating.get);
+	}
+
+}
