ef0d4d097e4a5ad1c0763a1d2f100671ff937b06
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-11-22 17:57:20 -0600

    Created entities with no foreign keys

--- RotahuJPA/src/main/java/com/skilldistillery/rotahu/entities/CreditResource.java
+++ RotahuJPA/src/main/java/com/skilldistillery/rotahu/entities/CreditResource.java
@@ -1,5 +1,127 @@
 package com.skilldistillery.rotahu.entities;
 
-public class CreditResource {
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="credit_resource")
+public class CreditResource {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String description;
+	
+	@Column(name="video_url")
+	private String videoUrl;
+	
+	@Column(name="site_url")
+	private String siteUrl;
+	
+	@Column(name="debt_intensity")
+	private Integer debtIntensity;
+	
+	@Column(name="created_at")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	private Boolean enabled;
+	
+	public CreditResource() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVideoUrl() {
+		return videoUrl;
+	}
+
+	public void setVideoUrl(String videoUrl) {
+		this.videoUrl = videoUrl;
+	}
+
+	public String getSiteUrl() {
+		return siteUrl;
+	}
+
+	public void setSiteUrl(String siteUrl) {
+		this.siteUrl = siteUrl;
+	}
+
+	public Integer getDebtIntensity() {
+		return debtIntensity;
+	}
+
+	public void setDebtIntensity(Integer debtIntensity) {
+		this.debtIntensity = debtIntensity;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CreditResource other = (CreditResource) obj;
+		return id == other.id;
+	}
+	
+	
 }
