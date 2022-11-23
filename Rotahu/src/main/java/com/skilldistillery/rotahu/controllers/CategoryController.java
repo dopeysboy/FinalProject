2f8eefe09b0b216c7c9bc88a47275c55f9d5e085
rubenarocha14 <rubenarocha14@gmail.com>
2022-11-23 17:02:17 -0600

    category controller

--- Rotahu/src/main/java/com/skilldistillery/rotahu/controllers/CategoryController.java
+++ Rotahu/src/main/java/com/skilldistillery/rotahu/controllers/CategoryController.java
@@ -1,10 +1,15 @@
 package com.skilldistillery.rotahu.controllers;
 
+import java.security.Principal;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.rotahu.entities.Category;
@@ -12,7 +17,7 @@ import com.skilldistillery.rotahu.services.CategoryService;
 
 
 @RestController
-@CrossOrigin({"*", "http://localhost"})
+@CrossOrigin({"*", "http://localhost/"})
 public class CategoryController {
 	
 	@Autowired
@@ -21,8 +26,24 @@ public class CategoryController {
 
 	
 	@GetMapping("category")
-	public List<Category> allCategories(){
-		return categoryService.findAll();
+	public List<Category> index(HttpServletRequest req, HttpServletResponse resp, Principal principal){
+		String username = principal.getName();
+		List<Category> todos = categoryService.findAll(username);
+		if(todos.size() == 0) {
+			resp.setStatus(404);
+		}
+		return todos;
+		
+	}
+	
+	@GetMapping("category/{categoryId}")
+	public Category findById(HttpServletRequest req, HttpServletResponse res, @PathVariable Integer categoryId, Principal principal) {
+		String username = principal.getName();
+		Category category = categoryService.findById(categoryId, username);
+		if(category == null) {
+			res.setStatus(404);
+		}
+		return category;
 		
 	}
 
