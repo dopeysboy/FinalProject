969b23587cb94f73901658ec935913298f26a0bc
rubenarocha14 <rubenarocha14@gmail.com>
2022-11-23 14:23:23 -0600

    debtType repo,serv,impl

---
+++ Rotahu/src/main/java/com/skilldistillery/rotahu/repositories/DebtTypeRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.rotahu.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.rotahu.entities.DebtType;
+
+public interface DebtTypeRepository extends JpaRepository <DebtType, Integer> {
+
+}
