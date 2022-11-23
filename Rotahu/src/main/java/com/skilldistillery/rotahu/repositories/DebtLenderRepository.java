1c8bd1ebcf33e1bbf38078b617ef72a2fc1f3f96
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-11-23 14:36:13 -0600

    DebtLenderService/Impl and repo completed

--- Rotahu/src/main/java/com/skilldistillery/rotahu/repositories/DebtLenderRepository.java
+++ Rotahu/src/main/java/com/skilldistillery/rotahu/repositories/DebtLenderRepository.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.rotahu.repositories;
 
-public interface DebtLenderRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.rotahu.entities.DebtLender;
+
+public interface DebtLenderRepository extends JpaRepository<DebtLender, Integer>{
+
+	List<DebtLender> findByNameContains(String keyword);
 }
