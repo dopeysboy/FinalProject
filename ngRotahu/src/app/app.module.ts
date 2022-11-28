f5375bd789d0fd53aa1ed092cc9d0363ca61031b
rubenarocha14 <rubenarocha14@gmail.com>
2022-11-28 11:56:18 -0700

    nav bar/ home component work

--- ngRotahu/src/app/app.module.ts
+++ ngRotahu/src/app/app.module.ts
@@ -5,26 +5,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { PublicCalulatorComponent } from './public-calulator/public-calulator.component';
-import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UserDebtComponent } from './user-debt/user-debt.component';
-import { ProfileComponent } from './profile/profile.component';
-import { CompanyDetailComponent } from './company-detail/company-detail.component';
-import { AdviceComponent } from './advice/advice.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PublicCalculatorComponent } from './components/public-calculator/public-calculator.component';
+import { UserDebtComponent } from './components/user-debt/user-debt.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
-    PublicCalulatorComponent,
+    HomeComponent,
     LoginComponent,
-    NavbarComponent,
+    PublicCalculatorComponent,
     UserDebtComponent,
+    NavbarComponent,
     ProfileComponent,
-    CompanyDetailComponent,
-    AdviceComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ ngRotahu/src/app/components/public-calculator/public-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-calculator',
+  templateUrl: './public-calculator.component.html',
+  styleUrls: ['./public-calculator.component.css']
+})
+export class PublicCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
