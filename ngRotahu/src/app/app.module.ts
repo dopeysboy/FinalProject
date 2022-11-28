f450f3f767269097513ffb5c5467ea486a6e462f
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-11-28 16:40:08 -0500

    fix app.module.ts imports

--- ngRotahu/src/app/app.module.ts
+++ ngRotahu/src/app/app.module.ts
@@ -5,15 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { PublicCalulatorComponent } from './public-calulator/public-calulator.component';
-import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UserDebtComponent } from './user-debt/user-debt.component';
-import { ProfileComponent } from './profile/profile.component';
-import { CompanyDetailComponent } from './company-detail/company-detail.component';
-import { AdviceComponent } from './advice/advice.component';
-import { RatingComponent } from './models/rating/rating.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PublicCalculatorComponent } from './components/public-calculator/public-calculator.component';
@@ -29,10 +20,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     PublicCalculatorComponent,
     UserDebtComponent,
     NavbarComponent,
-    ProfileComponent,
-    CompanyDetailComponent,
-    AdviceComponent,
-    RatingComponent
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
