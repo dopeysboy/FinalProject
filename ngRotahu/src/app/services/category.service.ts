7210fa7374ed0c4acbe7d75f509475e467baa862
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-11-28 12:05:35 -0600

    Finished required category, debtLender, and DebtType service methods

--- ngRotahu/src/app/services/category.service.ts
+++ ngRotahu/src/app/services/category.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Category } from '../models/category';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/category';
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.url, this.getHttpOptions()).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('CategoryService.index(): error retreiving Categories: ' + err)
+        );
+      })
+    );
+  }
+
+  getById(id: number): Observable<Category>{
+    return this.http.get<Category>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('CategoryService.getById(): error retreiving Category: ' + err)
+        );
+      })
+    );
+  }
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 }
--- ngRotahu/src/app/services/debt-type.service.ts
+++ ngRotahu/src/app/services/debt-type.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { DebtType } from '../models/debt-type';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DebtTypeService {
+  private url = environment.baseUrl + 'api/debttype';
 
-  constructor() { }
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<DebtType[]>{
+    return this.http.get<DebtType[]>(this.url, this.getHttpOptions()).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('DebtTypeService.index(): error retreiving DebtTypes: ' + err)
+        );
+      })
+    );
+  }
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 }
