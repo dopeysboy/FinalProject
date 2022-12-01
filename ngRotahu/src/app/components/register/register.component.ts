ef4318e91572363d83d9db4db536525445f63218
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-12-01 16:15:41 -0500

    reformat profile, register, logout; require matching retyped password to register

--- ngRotahu/src/app/components/register/register.component.ts
+++ ngRotahu/src/app/components/register/register.component.ts
@@ -10,34 +10,42 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
+  password1: string = '';
+  password2: string = '';
+  errorMessage: string = '';
 
   newUser: User = new User();
   constructor(private auth: AuthService,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.errorMessage = '';
   }
 
-  register(user: User): void {
-    console.log('Registering user:');
-    console.log(user);
-    this.auth.register(user).subscribe({
-      next: (registeredUser) => {
-        this.auth.login(user.username, user.password).subscribe({
-          next: (loggedInUser) => {
-            this.router.navigateByUrl('/home');
-          },
-          error: (problem) => {
-            console.error('RegisterComponent.register(): Error logging in user:');
-            console.error(problem);
-          }
-        });
-      },
-      error: (fail) => {
-        console.error('RegisterComponent.register(): Error registering account');
-        console.error(fail);
-      }
-    });
+  register(user: User, password1: string, password2: string): void {
+    if (password1 === password2) {
+      this.errorMessage = '';
+      user.username = password1;
+      this.auth.register(user).subscribe({
+        next: (registeredUser) => {
+          this.auth.login(user.username, user.password).subscribe({
+            next: (loggedInUser) => {
+              this.router.navigateByUrl('/home');
+            },
+            error: (problem) => {
+              console.error('RegisterComponent.register(): Error logging in user:');
+              console.error(problem);
+            }
+          });
+        },
+        error: (fail) => {
+          console.error('RegisterComponent.register(): Error registering account');
+          console.error(fail);
+        }
+      });
+    } else {
+      this.errorMessage = 'Passwords do not match';
+    }
   }
 
 
