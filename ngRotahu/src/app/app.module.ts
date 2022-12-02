ee5bd66321369da0d56ed114d9a2c7707a9fc45b
rubenarocha14 <rubenarocha14@gmail.com>
2022-12-02 13:17:26 -0700

    home and FAQ formatting

--- ngRotahu/src/app/app.module.ts
+++ ngRotahu/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { CreditResourceCrudComponent } from './components/credit-resource-crud/c
 import { UserPieChartComponent } from './components/user-pie-chart/user-pie-chart.component';
 import { UserCreatedCrComponent } from './components/user-created-cr/user-created-cr.component';
 import { ErrorComponent } from './components/error/error.component';
+import { CarouselPauseComponent } from './components/carousel-pause/carousel-pause.component';
 
 @NgModule({
   declarations: [
